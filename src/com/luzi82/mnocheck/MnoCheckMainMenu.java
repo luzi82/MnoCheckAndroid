3a12722c1ce177546b0dec7cf8f895d0ab9e81a3
luzi82 <luzi82@gmail.com>
2011-05-01 22:39:29 +0800

    init project

---
+++ src/com/luzi82/mnocheck/MnoCheckMainMenu.java
@@ -0,0 +1,13 @@
+package com.luzi82.mnocheck;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MnoCheckMainMenu extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
