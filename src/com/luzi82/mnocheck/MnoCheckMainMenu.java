95c26d0d499f98ff08609ec7a8f61abef2a12b01
luzi82 <luzi82@gmail.com>
2011-05-02 00:40:37 +0800

    Add about page (not full work)

--- src/com/luzi82/mnocheck/MnoCheckMainMenu.java
+++ src/com/luzi82/mnocheck/MnoCheckMainMenu.java
@@ -1,7 +1,11 @@
 package com.luzi82.mnocheck;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MnoCheckMainMenu extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +14,24 @@ public class MnoCheckMainMenu extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.mainmenu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_item_about: {
+			Intent intent = new Intent(this, AboutActivity.class);
+			startActivity(intent);
+			return true;
+		}
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 }
